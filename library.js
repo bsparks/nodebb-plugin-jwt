6675b1477a359dc7648c8b8140fd537da3089f73
Ben Sparks <bsparks@unicon.net>
2014-12-15 15:48:08 -0700

    initial commit attempting to add endpoint

---
+++ library.js
@@ -0,0 +1,48 @@
+(function (module) {
+    "use strict";
+
+    var User = module.parent.require('./user'),
+        meta = module.parent.require('./meta'),
+        jwt = module.parent.require('jsonwebtoken');
+
+    var constants = Object.freeze({
+        'name': "JWT",
+        'admin': {
+            'route': '/plugins/jwt',
+            'icon': 'fa-file-archive'
+        }
+    });
+
+    var JWT = {};
+
+    JWT.init = function (data, callback) {
+        function render(req, res, next) {
+            res.render('admin/plugins/jwt', {});
+        }
+
+        data.router.get('/admin/plugins/jwt', data.middleware.admin.buildHeader, render);
+        data.router.get('/api/admin/plugins/jwt', render);
+
+        function getToken(req, res, next) {
+            var token = jwt.sign(_.omit(req.user, 'password'), 'foo', { expiresInMinutes: 60*5 });
+
+            req.send(token);
+        }
+
+        data.router.get('/api/jwt', getToken);
+
+        callback();
+    };
+
+    JWT.addMenuItem = function (custom_header, callback) {
+        custom_header.authentication.push({
+            "route": constants.admin.route,
+            "icon": constants.admin.icon,
+            "name": constants.name
+        });
+
+        callback(null, custom_header);
+    };
+
+    module.exports = JWT;
+}(module));
